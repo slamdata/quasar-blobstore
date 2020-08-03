a044ebf912a45d0d9ad06c70dcd1c2ba1e4c8e47
Maxim Zimaliev <zimaliev@gmail.com>
2020-08-03 15:50:12 +0300

    quasar bump for external credentials

--- azure/src/main/scala/quasar/physical/blobstore/azure/AzureDatasourceModule.scala
+++ azure/src/main/scala/quasar/physical/blobstore/azure/AzureDatasourceModule.scala
@@ -21,11 +21,12 @@ import slamdata.Predef._
 import quasar.RateLimiting
 import quasar.api.datasource.{DatasourceError, DatasourceType}, DatasourceError._
 import quasar.blobstore.BlobstoreStatus
-import quasar.connector.{ByteStore, MonadResourceErr}
+import quasar.connector.{ByteStore, MonadResourceErr, ExternalCredentials}
 import quasar.connector.datasource.{LightweightDatasourceModule, Reconfiguration}
 import quasar.physical.blobstore.azure.json._
 
 import java.net.{MalformedURLException, UnknownHostException}
+import java.util.UUID
 import scala.concurrent.ExecutionContext
 import scala.util.control.NonFatal
 
@@ -46,7 +47,8 @@ object AzureDatasourceModule extends LightweightDatasourceModule {
       A: Hash](
       json: Json,
       rateLimiter: RateLimiting[F, A],
-      byteStore: ByteStore[F])(
+      byteStore: ByteStore[F],
+      getAuth: UUID => F[Option[ExternalCredentials[F]]])(
       implicit ec: ExecutionContext)
       : Resource[F, Either[InitializationError[Json], LightweightDatasourceModule.DS[F]]] = {
 
